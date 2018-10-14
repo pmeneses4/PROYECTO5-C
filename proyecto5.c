18fd934a27e895b0234ca1da28ae5281cd6d231e
pmeneses4 <pollo_desu@comunidad.unam.mx>
2018-10-14 02:14:15 -0500

    Primer commit

---
+++ proyecto5.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+struct disco$
+{
+	nombre char;
+	artista char;
+	precio int
+	añolanzamiento int;
+};
+
+struct usuario$
+{
+	nickname char;
+	contrasena char;
+	numcompras int;
+	cantidad int;
+};
+
+int main(int argc, char const *argv[])
+{
+	printf("Bienvenido a PrebeShop\n");
+	return 0;
+}
